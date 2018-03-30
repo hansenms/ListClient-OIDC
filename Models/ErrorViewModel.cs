33d527433c00387c54d43dbb8b5cac2218eeb2db
Michael Hansen <mihansen@microsoft.com>
2018-03-30 14:48:01 -0600

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ListClient_OIDC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
